efc7977c540854c2b6c0676785bc2eaac0d8c90f
oneku16 <eku.ulanov@yandex.com>
2022-02-03 22:11:43 +0600

    hw2_3

---
+++ UCA_Programming_II_HW/hw2_3.c
@@ -0,0 +1,23 @@
+// Write a C program that enters from the user three integer numbers and then 
+// calculates the maximum of three of them
+
+#include <stdio.h>
+
+int a, b, c;
+int maxnum = 0;
+
+
+int maxval(){
+
+	if((a > b) && (a > c)) maxnum = a;
+    else if(b > c)maxnum = b;
+    else maxnum = c;
+
+	return maxnum;
+}
+
+int main(){
+
+	scanf("%d %d %d", &a, &b, &c);
+	printf("%d", maxval());
+}
