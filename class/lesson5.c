2ca79263bfc2b05c31c17507e2345c34b306c7cd
oneku16 <eku.ulanov@yandex.com>
2022-02-08 11:09:26 +0600

    updated lesson5.c

--- class/lesson5.c
+++ class/lesson5.c
@@ -1,31 +1,37 @@
-#include <stdio.h>
-
-
-
 // ex 1
-// int main(){
-//     int a;
+// #include <stdio.h>
 
+// int main(){
+
+//     int a;
 //     scanf("%d", &a);
 
 //     if (a < 0) printf("a is neg\n");
 //     printf("a is: %d\n", a);
+
 //     return 0;
 // }
 
+// ________________________________________________________________________
 // ex 2
+// #include <stdio.h>
 
 // int main(){
+
 //     int a = 10;
 //     int b = -5;
 //     int c = 0;
+
 //     if (a) printf("True \n");
 //     if (b) printf("True \n");
 //     if (c) printf("False \n");
+
 //     return 0;
 // }
 
+// ________________________________________________________________________
 //ex 3
+// #include <stdio.h>
 
 // int main(){
 //     int a;
@@ -35,6 +41,44 @@
 //     printf("a is %d", a);
 //     return 0;
 // }
+
+// ________________________________________________________________________
+// ex 4
+// #include <stdio.h>
+
+// int main(){
+
+//     int a, b;
+//     scanf("%d %d", &a, &b);
+
+//     if(a == b)printf("res: %d = %d", a, b);
+//     else if (a > b) printf("res: %d > %d", a, b);
+//     else printf("res: %d < %d", a, b);
+
+//     return 0;
+// }
+
+// ________________________________________________________________________
+// ex 5
+
+
+#include <stdio.h>
+
+int main(){
+    
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    if(a >= b){
+
+        if (a == b)printf("res: %d = %d", a, b);
+        else printf("res: %d > %d", a, b);
+        
+    }
+    else printf("res: %d < %d", a, b);
+
+    return 0;
+}
 
 
 
