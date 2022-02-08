06886c36237c128bbd479c4cef40033099802f7f
oneku16 <eku.ulanov@yandex.com>
2022-02-08 10:37:21 +0600

    added lesson 5

---
+++ class/lesson5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+
+
+// ex 1
+// int main(){
+//     int a;
+
+//     scanf("%d", &a);
+
+//     if (a < 0) printf("a is neg\n");
+//     printf("a is: %d\n", a);
+//     return 0;
+// }
+
+// ex 2
+
+// int main(){
+//     int a = 10;
+//     int b = -5;
+//     int c = 0;
+//     if (a) printf("True \n");
+//     if (b) printf("True \n");
+//     if (c) printf("False \n");
+//     return 0;
+// }
+
+//ex 3
+
+// int main(){
+//     int a;
+//     scanf("%d", &a);
+//     if (a < 0)printf("a is less than 0\n");
+//     else printf("a is greater than %d\n", a);
+//     printf("a is %d", a);
+//     return 0;
+// }
+
+
+
