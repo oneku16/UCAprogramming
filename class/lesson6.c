93a8714a7f52ef6027d5cf21dde862a95b5175d2
oneku16 <eku.ulanov@yandex.com>
2022-02-09 10:38:42 +0600

    lesson6 was updated!

--- class/lesson6.c
+++ class/lesson6.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 
+int num;
 
+
+int main(){
+
+    scanf("%d",&num);
+
+    switch (num){
+
+        case 7:
+            printf("val is 7\n");
+            break;
+        
+        case 8:
+            printf("val is 8\n");
+            break;
+
+        case 9:
+            printf("val is 9\n");
+            break;
+
+        default:
+            printf("out of range\n");
+            break;    
+    }
+
+    return 0;
+}
 
 
 
