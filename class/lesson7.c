fd49499ce726732b61236759bb0dc6dce1e9dc15
oneku16 <eku.ulanov@yandex.com>
2022-02-15 16:20:46 +0600

    updated

--- class/lesson7.c
+++ class/lesson7.c
@@ -1,12 +1,40 @@
 // loop
 
+// ex 1
+// #include<stdio.h>
+
+// int main(){
+
+//     // here u can use postincrement and increment no difference
+//     for(int i=1; i <= 10; i++){
+
+//         printf("%d ", i);
+//     }
+//     return 0;
+// }
+
+
+// ______________________________________________________________________
+// ex 2
+
 #include<stdio.h>
+
+int num, cnt, sum = 0;
 
 int main(){
 
-    for(int i=1; i <= 10; i++){
+    printf("Enter num: ");
+    scanf("%d", &num);
 
-        printf("%d ", i);
+    for(cnt=1; cnt <= num; cnt++){
+
+        sum += cnt;
     }
+
+    printf("sum: %d\n", sum);
     return 0;
 }
+
+
+
+
