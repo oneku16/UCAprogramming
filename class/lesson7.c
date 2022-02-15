b4eca7d0f3527d71d180a53f912ea31013b31ecf
oneku16 <eku.ulanov@yandex.com>
2022-02-15 17:02:07 +0600

    updated less7

--- class/lesson7.c
+++ class/lesson7.c
@@ -17,21 +17,83 @@
 // ______________________________________________________________________
 // ex 2
 
-#include<stdio.h>
+// #include<stdio.h>
 
-int num, cnt, sum = 0;
+// int num, cnt, sum = 0;
+
+// int main(){
+
+//     printf("Enter num: ");
+//     scanf("%d", &num);
+
+//     for(cnt=1; cnt <= num; cnt++){
+
+//         sum += cnt;
+//     }
+
+//     printf("sum: %d\n", sum);
+//     return 0;
+// }
+
+// ______________________________________________________________________
+// ex 3
+
+
+// #include <stdio.h>
+
+// int num;
+
+// int main(){
+
+
+//     int i = 1;
+
+//     while(i <= 5){
+
+//         printf("%d\n",i);
+//         i++;
+//     }
+
+//     return 0;
+// }
+
+
+// ______________________________________________________________________
+// ex 4
+
+// #include <stdio.h>
+
+// double num, sum = 0;
+
+// int main(){
+//     do
+//     {
+//         printf("Enter a num: ");
+//         scanf("%lf",&num);
+//         sum += num;
+//     } while (num != 0.0);
+
+//     printf("sum = %.3f\n",sum);
+    
+//     return 0;
+// }
+
+
+// ______________________________________________________________________
+// ex 5
+
+#include <stdio.h>
+
 
 int main(){
 
-    printf("Enter num: ");
-    scanf("%d", &num);
+    for(int i=0; i < 2; i++){
 
-    for(cnt=1; cnt <= num; cnt++){
+        for(int j=0; j <4; j++){
 
-        sum += cnt;
+            printf("%d, %d\n", i, j);
+        }
     }
-
-    printf("sum: %d\n", sum);
     return 0;
 }
 
