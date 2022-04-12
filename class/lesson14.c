78a271f5f408237f718577ed27d62eb2f71e65db
oneku16 <eku.ulanov@yandex.com>
2022-04-12 17:12:33 +0600

    updated leeson14

--- class/lesson14.c
+++ class/lesson14.c
@@ -1,13 +1,42 @@
-#include<stdio.h>
+// ex 1 __________________________________________
 
+// #include<stdio.h>
+
+
+
+// int main(){
+
+//     printf("%s\n", __FILE__);
+//     printf("%s\n", __DATE__);
+//     printf("%s\n", __TIME__);
+//     printf("%d\n", __LINE__);
+//     printf("%d\n", __STDC__);
+//     return 0;
+// }
+
+// ex 2 __________________________________________
+
+// #include<stdio.h>
+// #define messenge(a, b)\
+//     printf(#a " and " #b ": We love you!\n");
+
+// int main(){
+//     messenge(1, 2);
+
+// }
+
+
+
+// ex 3 __________________________________________
+
+
+#include <stdio.h>
+
+#define CONCAT(a, b) a##b
 
 
 int main(){
 
-    printf("%s\n", __FILE__);
-    printf("%s\n", __DATE__);
-    printf("%s\n", __TIME__);
-    printf("%d\n", __LINE__);
-    printf("%d\n", __STDC__);
+    printf("%d", CONCAT(20,20));
     return 0;
 }
